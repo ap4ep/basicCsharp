9edb03316b4ee90d2f83e614ce396abb7866f58c
Ilya Naumov <ap4@live.ru>
2020-12-20 15:34:12 +0500

    1.4

---
+++ lesson1.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lesson1._4
+{
+    class Program
+    {
+        /// <summary>
+        /// Написать программу обмена значениями двух переменных:
+        ///  а) с использованием третьей переменной;
+        ///  б) *без использования третьей переменной.
+        ///  
+        ///  Наумов Илья
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Меняем переменые местами");
+            Console.WriteLine("Введите переменную а");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите переменную b");
+            int b = Int32.Parse(Console.ReadLine());
+            int t = 0;
+            Console.WriteLine($"a = {a}, b = {b}, t = {t}");
+            t = a; a = b; b = t;
+            Console.WriteLine($"Меняем местами  и получаем a = {a}, b = {b}, а так же осталась временная переменная t = {t}");
+
+            t = 0;
+            Console.WriteLine($"a = {a}, b = {b}, t = {0}");
+            (a, b) = (b, a);
+            Console.WriteLine($"Меняем местами используя кортежи и получаем a = {a}, b = {b}, а тут мы ее не трогали t = {t}");
+        }
+    }
+}
