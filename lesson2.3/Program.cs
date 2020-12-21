ae240c77e1c7a9a4a1c556cbf34ab7e42e8a3af4
Ilya Naumov <ap4@live.ru>
2020-12-21 22:08:48 +0500

    2.3

---
+++ lesson2.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lesson2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summary = 0;
+
+            while (true)
+            {
+                int number;
+                Console.WriteLine("Вводите числа:");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if(number > 0 && Convert.ToBoolean(number % 2))
+                {
+                    Console.WriteLine(summary);
+                    summary += number;
+                }
+                else
+                {
+                    if(number == 0)
+                    {
+                        Console.WriteLine($"Сумма нечетных чисел {summary}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
