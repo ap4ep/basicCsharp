4694db08dcb3b033916d7cc7328b3d177d876ad9
Ilya Naumov <ap4@live.ru>
2020-12-21 21:35:43 +0500

    2.1

---
+++ lesson2.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lesson2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimalValue(11, 20, 4));
+            Console.ReadKey();
+        }
+
+        private static int MinimalValue(int a, int b, int c)
+        {
+            int minimal;
+            if(a > b)
+            {
+                minimal = b;
+            }
+            else
+            {
+                minimal = a;
+            }
+
+            if(c < minimal)
+            {
+                minimal = c;
+            }
+
+            return minimal;
+        }
+    }
+}
