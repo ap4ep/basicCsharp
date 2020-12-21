b19604c2864640e813c04b21c16f7dce062772df
Ilya Naumov <ap4@live.ru>
2020-12-21 21:48:10 +0500

    2.2 and fix 2.1

--- lesson2.1/Program.cs
+++ lesson2.1/Program.cs
@@ -6,11 +6,11 @@ namespace lesson2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MinimalValue(11, 20, 4));
+            Console.WriteLine(GetMinimalValue(11, 20, 4));
             Console.ReadKey();
         }
 
-        private static int MinimalValue(int a, int b, int c)
+        private static int GetMinimalValue(int a, int b, int c)
         {
             int minimal;
             if(a > b)
---
+++ lesson2.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lesson2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetNumberCount(123456789));
+            Console.ReadKey();
+        }
+
+        private static int GetNumberCount(int x)
+        {
+            int count = 0; 
+
+            while(x != 0)
+            {
+                count++;
+                x /= 10;
+            }
+
+            return count;
+        }
+    }
+}
