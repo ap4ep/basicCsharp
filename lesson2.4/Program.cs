b8cda8f366c4ca06305eb6b962863058b42333ba
Ilya Naumov <ap4@live.ru>
2020-12-21 22:32:11 +0500

    2.4

---
+++ lesson2.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lesson2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Authoriaztion());
+        }
+
+        static bool Authoriaztion()
+        {
+            int tryCount = 0;
+            string login;
+            string password;
+
+            do
+            {
+                tryCount++;
+                Console.Write("Логин:");
+                login = Console.ReadLine();
+                Console.Write("Пароль:");
+                password = Console.ReadLine();
+
+                if (login == "root")
+                {
+                    if (password == "GeekBrains")
+                        return true;
+                    else
+                        tryCount++;
+                }
+            } while (tryCount < 3);
+
+            return false;
+        }
+    }
+}
