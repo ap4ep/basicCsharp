d10f8b39454cff5cb940c4430b32cdae09bf3096
Ilya Naumov <ap4@live.ru>
2020-12-28 23:54:55 +0500

    4.1

---
+++ lesson4.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lesson4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] array = new int[20];
+            int pairs = 0;
+
+            for(int i = 0; i <= array.Length - 1; i++)
+            {
+                array[i] = rnd.Next(-10000, 10000);
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[i] % 3 == 0 && i < array.Length - 1)
+                {
+                    if(array[i + 1] % 3 != 0 )
+                    {
+                        pairs++;
+                    }
+                }
+                
+                if (array[i] % 3 != 0 && i < array.Length - 1)
+                {
+                    if (array[i + 1] % 3 == 0)
+                    {
+                        pairs++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{pairs} пар элементов");
+        }
+
+        
+    }
+}
