a2a0eef56e32f76282ae5e7434b5e93ecc8d2aff
Ilya Naumov <ap4@live.ru>
2020-12-28 22:00:10 +0500

    3.1

---
+++ lesson3.1/Complex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Complex
+    {
+        public double a, b;
+
+        public Complex(double v1, double v2)  
+        {
+            a = v1;
+            b = v2;
+        }
+
+        public void Print()
+        {
+            string temp = String.Empty;
+
+            if (a == 0) temp = $"";
+            else temp = $"{a}";
+
+            if (b == 0) temp += $"";
+            else if (b > 0) temp += $"+ {b}i";
+            else temp += $" - {-b}i";
+            if (a == 0 && b == 0) temp = "0";
+            Console.WriteLine(temp);
+        }
+
+        public static Complex Addition(Complex z1, Complex z2)
+        {
+            Complex result = new Complex(z1.a + z2.a, z1.b + z2.b);
+
+            return result;
+        }
+
+        public static Complex Subtraction(Complex z1, Complex z2)
+        {
+            Complex result = new Complex(z1.a - z2.a, z1.b - z2.b);
+
+            return result;
+        }
+
+        public static Complex Multiplication(Complex z1, Complex z2)
+        {
+            Complex result = new Complex(z1.a * z2.a - z1.b * z2.b, z1.a * z2.b + z2.a * z1.b);
+
+            return result;
+        }
+    }
+}
---
+++ lesson3.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        /// <summary>
+        /// а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.
+        ///б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+        ///в) Добавить диалог с использованием switch демонстрирующий работу класса.
+        ///
+        /// Наумов Илья
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Введите первое комплексное число:");
+            Complex z1 = new Complex(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Введите второе комплексное число:");
+            Complex z2 = new Complex(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+
+            z1.Print();
+            z2.Print();
+
+            Console.WriteLine("Введите операцию над числами(+, -, *)");
+            switch(Console.ReadLine())
+            {
+                case "+":
+                    Complex a = Complex.Addition(z1, z2);
+                    a.Print();
+                    break;
+                case "-":
+                    Complex s = Complex.Subtraction(z1, z2);
+                    s.Print();
+                    break;
+                case "*":
+                    Complex m = Complex.Multiplication(z1, z2);
+                    m.Print();
+                    break;
+                default:
+                    Console.WriteLine("Нет такой операции");
+                    break;
+            }
+        }
+    }
+}
